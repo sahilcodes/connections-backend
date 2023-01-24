131d06b867056043d84c8ec4c900ca3c485fe22b
sahilcodes <sahilcancode@gmail.com>
2023-01-24 12:37:10 +0530

    Changed absolute paths and build script

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express";
-import { ConnServer } from "./setupServer";
-import databaseConnection from "./setupDatabase";
-import { config } from "./config";
+import { ConnServer } from "@root/setupServer";
+import databaseConnection from "@root/setupDatabase";
+import { config } from "@root/config";
 
 class Application {
   public initialize(): void {
